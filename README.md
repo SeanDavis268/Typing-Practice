623303f59a96234c61530d1e9fa4aabff7b4eb2e
SeanDavis268 <51014235+SeanDavis268@users.noreply.github.com>
2019-09-28 16:55:53 -0500

    txt files can now be used
    They are read one line at a time and displayed one line at a time as well.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Typing Practice
- This program takes a txt doc and uses it for practice
+ This program takes a txt doc and uses it for typing practice and studying.
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import os
+
 
 
 
@@ -14,17 +16,59 @@ class main():
 
         self.start= Frame(self.top)
         self.start.pack()
-        self.phrase='how much would could a woodchuck chuck'
+        self.phrase='the phrase didn"t load' #default phrase
+        ####file reading#####
+        cwd=os.getcwd()
+        textFiles = [f for f in os.listdir(cwd) if f.endswith('.txt')]
+
+        #i=-1
+        #for each in textFiles:
+            #file=open(each,'r')
+            #Files2Read.append(file)
+            #i+=1
+            #Button(self.start, text=each, command=lambda:self.phraseGen(i,textFiles)).pack() #for some reason it always loads the text as the last txt docs name
+            #this is likely because when the button is pushed it reads the last each and the txt for the button reads it as its iterated
+
+
+        Label(self.start, text=textFiles).pack()
+        entryBox=Text(self.start,width = '10', height = '1')
+        myList=[]
+        myList.append(entryBox)
+        entryBox.pack()
+        widget=Button(self.start,text='click to load file', command=lambda:self.phraseGen(entryBox.get('1.0',END),textFiles))
+        widget.pack()
+
         Button(self.start,text='woodchuck',command=lambda:self.phraseGen('how much would could a woodchuck chuck')).pack()
         Button(self.start,text='tounge tied',command=lambda:self.phraseGen('Sally sells sea shells down by the sea shore')).pack()
+
+
+
         self.start.mainloop()
 
 
 
 
-    def phraseGen(self,txt):
-        self.phrase=txt
-        print(self.phrase)
+    def phraseGen(self,txt,container=False):
+        cwd=os.getcwd()
+        files= [f for f in os.listdir(cwd) if f.endswith('.txt')]
+        #print(files)
+        print(txt)
+        yeet=str(txt[0:-1])
+        self.txt=txt
+        try:
+            #print(container)
+             #IT currently won't accept any input as correct
+            print(yeet)
+            self.file=open(yeet,'r')
+            print('loaded')
+            self.txt=self.file.readline()
+            print(txt)
+
+        except:
+            print('failed')
+
+        self.phrase=self.txt
+        #print(self.phrase)
         self.start.destroy()
         self.phase2()
 
@@ -40,7 +84,7 @@ class main():
 
         #canvas=Canvas(self.win)
         #canvas.pack()
-        self.text=Text(self.win, height=25, width=60)
+        self.text=Text(self.win, height=25, width=65)
         self.text.insert('end',self.phrase)
         self.text['state']=DISABLED
         self.text.pack()
@@ -57,6 +101,8 @@ class main():
         temp=str(event.char)
         #print(temp)
         wanted=self.phrase[len(self.userP)]
+
+
         if temp==wanted:
             print(temp)
             self.userP=self.userP+temp
@@ -64,11 +110,28 @@ class main():
         else:
             self.error+=1
 
+        if len(self.phrase)-1==len(self.userP):
+            print('########################')
+            self.txt=self.file.readline()
+            self.phrase=self.txt
+            print(self.phrase)
+            self.text.config(state=NORMAL)
+            self.text.delete('1.0','end')
+            self.text.insert('1.0',self.phrase)
+            self.text.config(state=DISABLED)
+
+
+
+
+            #self.phrase=  #clear the data
+            self.userP=''
+
+
     def highlight(self,text):
-        wanted=len(self.userP)-1
+        wanted=len(self.userP)
 
         text.tag_delete('red')
-        
+
 
         txtInput=(str(1)+'.'+str(wanted))
         print(txtInput)
